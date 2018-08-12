a8865a12a62013db8f22997fc3aa86a4db8ac60e
Marshal13 <moje.gryigites@gmail.com>
2018-08-12 10:53:49 +0200

    sx

---
+++ odliczanie_wsteczne.py
@@ -0,0 +1,24 @@
+"""
+name = 'ilonia'
+word = ""
+
+change = list(name)
+
+change.reverse()
+
+for symbol in change:
+    word += symbol
+print(word)
+"""
+
+napis = "rudy list przeskoczył nad śpiącym wilczurem"
+slowa = napis.split()
+
+for slowo in slowa:
+    print(len(slowo))
+    
+
+
+    
+    
+
